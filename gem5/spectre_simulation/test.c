5979c9bd80bb582e657658e09a8f471e8100d5f1
Demario-Huang <119010108@link.cuhk.edu.cn>
2022-11-09 10:13:53 +0800

    add the simple gem5 system

---
+++ gem5/spectre_simulation/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc , char ** argv){
+  int a = 10; 
+  int b = 13;
+  int c = (a==b);
+  printf("c is %d \n", c);
+  printf("hello world!\n");
+  return 0;
+}
